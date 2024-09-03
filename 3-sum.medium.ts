c76915eb110f5e00e4b3e7fa59b496aef99220ce
mdkmridul <eemridulhemani@gmail.com>
2024-09-04 00:29:48 +0530

    - #sum 2nd Submission

--- 3-sum.medium.ts
+++ 3-sum.medium.ts
@@ -39,3 +39,44 @@ function threeSum(nums: number[]): number[][] {
     }
     return result
 };
+
+// 2nd Submission
+    // - should check for the sum first rather than having it at the end, that will make us miss some of the combinations
+    // - use while loop to move pointer on the l & r so that other operations are stopped while we do it, which will be the case if we have just if condition inside the first while loop
+    // - capture values before moving any pointers, so try to match with last value rather than matching with the next one
+
+function threeSumUpdated(nums: number[]): number[][] {
+    nums = nums.sort((a,b) => a-b)
+
+    let l,r;
+    const result: number[][] = []
+    for (let i = 0; i < nums.length; i++) {
+        if(nums[i] > 0) break
+        if(i > 0 && nums[i] === nums[i-1]) {
+            continue
+        }
+
+        l = i + 1
+        r = nums.length - 1
+
+        while (l < r){
+            if(nums[i] + nums[l] + nums[r] === 0) {
+                result.push([nums[i], nums[l], nums[r]])
+                r -= 1
+                l += 1
+                while (l < r && nums[l] === nums[l-1]) {
+                    l += 1
+                }
+                while (l < r && nums[r] === nums[r+1]) {
+                    r -= 1
+                }
+            } else if(nums[i] + nums[l] + nums[r] > 0) {
+                r -= 1
+            } else {
+                l += 1
+            }
+            
+        }
+    }
+    return result
+};
